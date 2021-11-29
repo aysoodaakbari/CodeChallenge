18268fe03997e9b76afd895861718df6d60e0133
aysoodaakbari <aysoodaakbariii5515@gmail.com>
2021-11-29 18:19:00 +0330

    check window connection

--- day-2/main.js
+++ day-2/main.js
@@ -2,35 +2,49 @@ const form = document.querySelector('#task-form');
 const input = document.querySelector('#task-input');
 const list = document.querySelector('#tasks');
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const task = input.value;
-    if (task == null) {
-        alert("please fill the task input")
-        return;
-    }
-    const task_el = document.createElement('div');
-    task_el.classList.add("task");
-    const task_contentElement = document.createElement("div");
-    task_contentElement.classList.add('content');
-    task_contentElement.innerHTML = task;
-    const task_actionsElement = document.createElement('div');
-    task_actionsElement.classList.add('actions');
+        e.preventDefault();
+        const task = input.value;
+        if (task == null) {
+            alert("please fill the task input")
+            return;
+        }
+        const task_el = document.createElement('div');
+        task_el.classList.add("task");
+        const task_contentElement = document.createElement("div");
+        task_contentElement.classList.add('content');
+        task_contentElement.innerHTML = task;
+        const task_actionsElement = document.createElement('div');
+        task_actionsElement.classList.add('actions');
 
-    const task_editElement = document.createElement('button');
-    task_editElement.classList.add('edit');
-    task_editElement.innerText = 'Edit';
+        const task_editElement = document.createElement('button');
+        task_editElement.classList.add('edit');
+        task_editElement.innerText = 'Edit';
 
-    const task_delete_el = document.createElement('button');
-    task_delete_el.classList.add('delete');
-    task_delete_el.innerText = 'Delete';
-    task_el.appendChild(task_actionsElement);
+        const task_delete_el = document.createElement('button');
+        task_delete_el.classList.add('delete');
+        task_delete_el.innerText = 'Delete';
+        task_el.appendChild(task_actionsElement);
 
-    task_actionsElement.appendChild(task_editElement);
-    task_actionsElement.appendChild(task_delete_el);
+        task_actionsElement.appendChild(task_editElement);
+        task_actionsElement.appendChild(task_delete_el);
 
-    task_el.appendChild(task_contentElement);
-    list.appendChild(task_el);
+        task_el.appendChild(task_contentElement);
+        list.appendChild(task_el);
 
 
 
-})
+    })
+    //wifi connection
+let el = document.createElement("div");
+document.querySelector('header').prepend(el);
+
+window.addEventListener('offline', (event) => {
+    el.textContent = 'offline';
+    el.className = 'alert';
+    el.classList.add('alert-danger')
+});
+window.addEventListener('online', (event) => {
+    el.textContent = 'online';
+    el.className = 'alert';
+    el.classList.add('alert-danger')
+});
